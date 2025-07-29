6f70fb7683e85a1c7df2f51b2c8fb3feaf2e4cb9
Federico Gabusi <federico.gabusi@marchesini.com>
2025-07-29 12:24:55 +0200

    build fix

--- build.zig
+++ build.zig
@@ -7,11 +7,8 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.resolveTargetQuery(.{
-        .cpu_arch = .arm,
-        .abi = .eabi,
-    });
 
+    const target = b.standardTargetOptions(.{});
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
